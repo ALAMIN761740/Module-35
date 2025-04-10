f23f72a2a18d0a2df7310df11c35adbcb854af09
ALAMIN761740 <abdullahalamin761740@gmail.com>
2025-04-10 10:10:55 +0600

    The end

--- Script/index.js
+++ Script/index.js
@@ -1,5 +1,15 @@
 
 
+function removeActiveClass(){
+    const activeButtons = document.getElementsByClassName("active");
+
+    for (let btn of activeButtons){
+        btn.classList.remove("active");
+    }
+    console.log(activeButtons);
+}
+
+
 // button -----------------fetch
 function loadCategories() {
     // 1- fetch the data
@@ -11,11 +21,16 @@ function loadCategories() {
 }
 
 // video -----------fetch
-function loadVideos (){
-    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+function loadVideos ( searchText = ""){
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then((res)=>res.json())
-    .then((data)=>displayVideos(data.videos) );
+    .then((data)=>{
+        document.getElementById("btn-All").classList.add("active");
+        displayVideos(data.videos)
+    } );
 }
+
+
 
 const loadCategoriesVideos = (id) => {
     const url =`
@@ -26,11 +41,55 @@ const loadCategoriesVideos = (id) => {
 
     fetch(url)
         .then((res)=> res.json())
-        .then((data)=> displayVideos(data.category));
+        .then((data)=> { 
+            removeActiveClass();
+            const clickedButton = document.getElementById(`btn-${id}`)
+            clickedButton.classList.add("active");
+            displayVideos(data.category);
+            
+        });
 };
 
 
 
+// -----------------------------------------------
+const loadVideoDetails =(videoId)=>{
+const url = `
+https://openapi.programming-hero.com/api/phero-tube/video/${videoId}
+`;
+fetch(url)
+.then(res=>res.json())
+.then((data)=> displayVideosDetails (data.video));
+};
+
+const displayVideosDetails=(video)=>{
+    document.getElementById("Video_Details").showModal();
+    const detailsContainer = document.getElementById("details_container");
+
+    detailsContainer.innerHTML=`
+    <div class="card bg-base-100 image-full  shadow-sm">
+        <figure>
+            <img
+            src="${video.thumbnail}"
+            alt="Shoes" />
+        </figure>
+    <div class="card-body">
+        <h2 class="text-2xl">${video.title}</h2>
+        <p>${video.authors[0].profile_name}</p>
+        <p>${video.description}</p>
+        <div class="card-actions justify-end">
+        
+        </div>
+    </div>
+    </div>
+
+    
+    `;
+};
+
+
+
+// -----------------------------------
 
 
 // button-----------
@@ -45,7 +104,7 @@ function displayCategories (categories){
         // create Element
         const categoryDiv = document.createElement("div");
         categoryDiv.innerHTML=`
-        <button onclick="loadCategoriesVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+        <button id="btn-${cat.category_id}" onclick="loadCategoriesVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
         `;
 
         // append the element
@@ -62,6 +121,16 @@ const displayVideos = (videos) => {
     const videoContainer = document.getElementById("Video-Container");
 
     videoContainer.innerHTML ="";
+
+    if(videos.length==0){
+        videoContainer.innerHTML =`
+        <div class="col-span-full flex flex-col justify-center items-center text-center py-20">
+            <img class="w-[120px]" src="Assets/Icon.png" alt="">
+            <h2 class="text-2xl font-bold">Oops!! Sorry, There is no content here</h2>
+        </div>
+        `;
+        return;
+    }
 
     videos.forEach((video) => {
         // console.log(video);
@@ -86,10 +155,14 @@ const displayVideos = (videos) => {
 
                 <div class="intro">
                     <h2 class="text-lg font-semibold">Midnight Serenade</h2>
-                    <p class="text-sm text-gray-400 flex gap-2">${video.authors[0].profile_name}<img class="w-6 " src="https://img.icons8.com/?size=32&id=AOpCOemSYvTO&format=png" alt=""></p>
+                    <p class="text-sm text-gray-400 flex gap-2">${video.authors[0].profile_name}
+                    
+                    ${video.authors[0].verified == true ? `<img class="w-6  " src="https://img.icons8.com/?size=32&id=AOpCOemSYvTO&format=png" alt="">` : `` }
+                    </p>
                     <p class="text-sm text-gray-400">${video.others.views}</p>
                 </div>
             </div>
+            <button onclick=loadVideoDetails("${video.video_id}") class="btn btn-block hover:bg-[#FF1F3D] hover:text-white">Show Details</button>
         </div>
 
         `;
@@ -97,6 +170,12 @@ const displayVideos = (videos) => {
         videoContainer.append(videoCard);
     });
 }
+
+
+document.getElementById("search-input").addEventListener("keyup",(e)=>{
+    const input = e.target.value;
+    loadVideos(input);
+});
 
 
 
